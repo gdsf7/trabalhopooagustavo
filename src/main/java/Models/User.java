bfd97745601f6b945d5c57f6a40c0fdeebb79586
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2025-11-17 19:51:12 -0300

    User feito

---
+++ src/main/java/Models/User.java
@@ -0,0 +1,38 @@
+package Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Date;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable=false, length=50)
+    private String nome;
+    @Column(nullable = false)
+    private String sobrenome;
+    @Column(nullable = false)
+    private String senha;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(unique = true)
+    private String telefone;
+    @Column(nullable = false, unique = true)
+    private int matricula;
+    @Column(nullable = false)
+    private int funcao;
+
+
+
+
+
+
+}
