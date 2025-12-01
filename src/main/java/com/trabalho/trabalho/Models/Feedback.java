24f1bdb04b384d3ba2bd25740a8314eb30ee6386
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2025-12-01 19:58:19 -0300

    coisas do user e localdate feitos

--- src/main/java/com/trabalho/trabalho/Models/Feedback.java
+++ src/main/java/com/trabalho/trabalho/Models/Feedback.java
@@ -3,6 +3,9 @@ package com.trabalho.trabalho.Models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Builder
 @Getter
 @Setter
@@ -31,10 +34,10 @@ public class Feedback {
     @Column
     private String resp_familia;
 
-    // depois eu ponho a data
+    @Column
+    private LocalDate resp_data;
 
     @ManyToOne
     @JoinColumn(name = "idGuardiao", nullable = false)
     private User guardiao;
-
 }
