4b43e3732b9210d5a273c10594a1c10833c0a608
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2025-12-01 16:01:23 -0300

    historico feito

---
+++ src/main/java/Models/Historico.java
@@ -0,0 +1,27 @@
+package Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Historico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "idAluno", nullable = false)
+    private User aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "idProfessor", nullable = false)
+    private User professor;
+}
