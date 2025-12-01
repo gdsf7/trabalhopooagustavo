f6bbaad8ffa244074e011d8e7311fd7e86924234
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2025-12-01 16:04:47 -0300

    repositories feitos

---
+++ src/main/java/Models/DisciplinaRepository.java
@@ -0,0 +1,8 @@
+package Models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DisciplinaRepository extends JpaRepository<User, Long> {
+}
